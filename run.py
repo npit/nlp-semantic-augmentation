c8e055dbd2658869fe242439960aadd0f46f9566
npit <pittarasnikif@gmail.com>
2018-10-04 01:27:28 +0300

    Add planning

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import keras
 import numpy as np
 
+print("Imports done.")
 #  wordnet api in nltk
 from nltk.corpora import wordnet as wn
 
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# activate virtualenv
+venv="../venv/bin/activate"
+source "${venv}"
+
+# run
+python run.py
