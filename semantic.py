0df5b8eb31775cdd4092409f62352866a467d871
npit <pittarasnikif@gmail.com>
2018-11-19 16:42:04 +0200

    Add babelnet skeleton

--- semantic.py
+++ semantic.py
@@ -715,4 +715,22 @@ class Framenet(SemanticResource):
                     activations = {**activations, **parents}
         return activations
 
+class BabelNet:
+    name = "babelnet"
+    def get_raw_path(self):
+        return None
+
+    def __init__(self, config):
+        self.config = config
+        self.base_name = self.name
+        SemanticResource.__init__(self, config)
+        # map nltk pos maps into meaningful framenet ones
+        self.pos_tag_mapping = {"VB": "V", "NN": "N", "JJ": "A", "RB": "ADV"}
+
+    # lookup for babelnet should be about a (large) set of words
+    # written into a file, read by the java api
+    # results written into a file (json), read from here.
+    # run calls the java program
+
+
 
