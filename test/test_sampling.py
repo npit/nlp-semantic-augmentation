bc414ab88f4033d92c3d6b474599e57d7fbc53be
npit <pittarasnikif@gmail.com>
2021-04-10 20:28:15 +0300

    --amend

---
+++ test/test_sampling.py
@@ -0,0 +1,9 @@
+from sampling.sampling import Sampler
+from utils import *
+
+def test_labelling_consistency(self):
+    c = {"min_freq": 3, "max_freq": 10}
+    s = Sampler(to_namedtuple(c))
+    assert True, ""
+    assert False, "bork"
+
